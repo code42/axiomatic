d1a0e0a773dd804a4f28edbf8b940d28e9a5de2b
Jim Razmus II <jim.razmus@code42.com>
2020-02-03 21:39:40 -0600

    Add rough draft version.

---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/google/go-github/github"
+)
+
+// conditionally compile in or out the debug prints
+const debug = false
+
+// AxiomaticIP is the IP address to bind
+var AxiomaticIP = getenv("AXIOMATIC_IP", "")
+
+// AxiomaticPort is the port number to bind
+var AxiomaticPort = getenv("AXIOMATIC_PORT", "8181")
+
+// GithubWebhookSecret is the secret token for validating webhook requests
+var GithubWebhookSecret = getenv("GITHUB_SECRET", "you-deserve-what-you-get")
+
+// NomadServerURL is the URL of the Nomad server that will handle job submissions
+var NomadServerURL = getenv("NOMAD_SERVER", "http://localhost:4646")
+
+// NomadJobData contains data for job template rendering
+type NomadJobData struct {
+	GitRepoURL string
+	HeadSHA    string
+	Name       string
+}
+
+func main() {
+	// xxx: add command line flags support
+	log.Println("Axiomatic Server Starting")
+	http.HandleFunc("/health", handleHealth)
+	http.HandleFunc("/webhook", handleWebhook)
+	serverAddr := strings.Join([]string{AxiomaticIP, AxiomaticPort}, ":")
+	log.Fatal(http.ListenAndServe(serverAddr, nil))
+	return
+}
+
+// getenv returns the environment value for the given key or the default value when not found
+func getenv(key string, _default string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return _default
+	}
+	return val
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	log.Println("Good to Serve")
+	fmt.Fprintln(w, "Good to Serve")
+	return
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	payload, err := github.ValidatePayload(r, []byte(GithubWebhookSecret))
+	if err != nil {
+		log.Printf("error validating request body: err=%s\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	event, err := github.ParseWebHook(github.WebHookType(r), payload)
+	if err != nil {
+		log.Printf("could not parse webhook: err=%s\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	switch e := event.(type) {
+	case *github.PingEvent:
+		log.Println("GitHub Pinged the Webhook")
+	case *github.PushEvent:
+		jobName := strings.Join([]string{"axiomatic", e.Repo.GetFullName()}, "-")
+
+		jobArgs := NomadJobData{
+			GitRepoURL: e.Repo.GetCloneURL(),
+			HeadSHA:    e.GetAfter(),
+			Name:       jobName,
+		}
+		if debug {
+			log.Printf("jobArgs: %+v\n", jobArgs)
+		}
+
+		jobText, err := renderNomadJob(jobArgs)
+		if err != nil {
+			log.Println("renderNomamdJob Error:", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if debug {
+			log.Println("jobText:", jobText)
+		}
+
+		err = submitNomadJob(jobName, jobText)
+		if err != nil {
+			log.Println("submitJob Error:", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		log.Println("submitJob Success:", jobName)
+		fmt.Fprintln(w, "Nomad Job Submitted")
+	default:
+		log.Printf("WARN: unknown event type %s\n", github.WebHookType(r))
+		return
+	}
+}
+
+// renderNomadJob combines a template with supplied args and returns a Nomad job definition as a string
+func renderNomadJob(jobArgs NomadJobData) (*bytes.Buffer, error) {
+	t := template.Must(template.New("job").Parse(jobTemplate))
+	buf := &bytes.Buffer{}
+	err := t.Execute(buf, jobArgs)
+	if err != nil {
+		return buf, err
+	}
+	return buf, nil
+}
+
+// submitNomadJob sends a job to a Nomad server REST API
+func submitNomadJob(jobName string, jobBody *bytes.Buffer) error {
+	url := strings.Join([]string{NomadServerURL, "v1/job", jobName}, "/")
+	if debug {
+		log.Println("URL:", url)
+	}
+
+	request, err := http.NewRequest("POST", url, jobBody)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if debug {
+		body, _ := ioutil.ReadAll(response.Body)
+		log.Println("response Body:", string(body))
+	}
+
+	if response.StatusCode < 200 || response.StatusCode > 299 {
+		return errors.New(response.Status)
+	}
+	return nil
+}
